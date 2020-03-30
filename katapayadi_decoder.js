eb070a79223816b08b3b988a585a0a6c0c3415f6
Vinayaka K V <me.vinayakakv@gmail.com>
2020-03-30 10:01:22 +0530

    Update `akshara_tokenizer` to v1.0.0

--- katapayadi_decoder.js
+++ katapayadi_decoder.js
@@ -1,4 +1,4 @@
-import {AksharaTokenizerKannada} from 'https://cdn.jsdelivr.net/gh/vinayakakv/akshara_tokenizer/akshara_tokenizer.js'
+import {AksharaTokenizerKannada} from 'https://cdn.jsdelivr.net/gh/vinayakakv/akshara_tokenizer@1.0.0/akshara_tokenizer.js'
 
 function katapayadi_decode(string) {
     let tokenizer = new AksharaTokenizerKannada();
@@ -6,9 +6,9 @@ function katapayadi_decode(string) {
     let ord = c => c.charCodeAt(0);
     let mapper = char_dict => {
         let value = '0';
-        if (char_dict['vyanjana']) {
-            let char = ord(char_dict['vyanjana'])
-            if (char_dict['halant'])
+        if (char_dict['vyamjana']) {
+            let char = ord(char_dict['vyamjana'])
+            if (char_dict['virama'])
                 value = '';
             else if (char >= ord('ಕ') && char < ord('ಞ'))
                 value = char - ord('ಕ') + 1
